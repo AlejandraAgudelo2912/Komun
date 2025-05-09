918d363d45cb728184868b3f07377633f4a16d04
LAPTOP-1DQNJ8L3\aleja <339210@alu.murciaeduca.es>
2025-05-09 02:11:25 +0200

    Paso 4. Redireccionar las rutas de los roles

---
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\LoginRequest;
+use App\Providers\RouteServiceProvider;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
+class AuthenticatedSessionController extends Controller
+{
+    /**
+     * Display the login view.
+     *
+     * @return \Illuminate\View\View
+     */
+    public function create()
+    {
+        return view('auth.login');
+    }
+
+    /**
+     * Handle an incoming authentication request.
+     *
+     * @param  \App\Http\Requests\Auth\LoginRequest  $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function store(LoginRequest $request): RedirectResponse
+    {
+        $request->authenticate();
+        $request->session()->regenerate();
+
+        $user = auth()->user();
+
+        if ($user->hasRole('admin')) {
+            return redirect()->route('admin.dashboard');
+        } elseif ($user->hasRole('god')) {
+            return redirect()->route('god.dashboard');
+        } elseif ($user->hasRole('verificator')) {
+            return redirect()->route('verificator.dashboard');
+        } elseif ($user->hasRole('assistant')) {
+            return redirect()->route('assistant.dashboard');
+        } elseif ($user->hasRole('needHelp')) {
+            return redirect()->route('needhelp.dashboard');
+        }
+
+        return redirect()->intended(RouteServiceProvider::HOME);
+    }
+
+    /**
+     * Destroy an authenticated session.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function destroy(Request $request)
+    {
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('/');
+    }
+} 
--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,33 @@ Route::middleware([
         return view('dashboard');
     })->name('dashboard');
 });
+
+Route::middleware(['auth', 'role:admin'])->group(function () {
+    Route::get('/admin/dashboard', function () {
+        return view('admin.dashboard');
+    })->name('admin.dashboard');
+});
+
+Route::middleware(['auth', 'role:god'])->group(function () {
+    Route::get('/god/dashboard', function () {
+        return view('god.dashboard');
+    })->name('god.dashboard');
+});
+
+Route::middleware(['auth', 'role:verificator'])->group(function () {
+    Route::get('/verificator/dashboard', function () {
+        return view('verificator.dashboard');
+    })->name('verificator.dashboard');
+});
+
+Route::middleware(['auth', 'role:assistant'])->group(function () {
+    Route::get('/assistant/dashboard', function () {
+        return view('assistant.dashboard');
+    })->name('assistant.dashboard');
+});
+
+Route::middleware(['auth', 'role:needHelp'])->group(function () {
+    Route::get('/needhelp/dashboard', function () {
+        return view('needhelp.dashboard');
+    })->name('needhelp.dashboard');
+});
