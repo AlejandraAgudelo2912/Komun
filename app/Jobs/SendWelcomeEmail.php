fcbafd3a32c784b46b987b2c80479fb6e067eb9b
LAPTOP-1DQNJ8L3\aleja <339210@alu.murciaeduca.es>
2025-05-11 11:15:58 +0200

    Fix

--- app/Jobs/SendWelcomeEmail.php
+++ app/Jobs/SendWelcomeEmail.php
@@ -24,19 +24,9 @@ class SendWelcomeEmail implements ShouldQueue
         //
     }
 
-    /**
-     * Execute the job.
-     */
+    
     public function handle(): void
     {
-        Log::info('Enviando correo de bienvenida a: ' . $this->user->email);
         
-        try {
-            Mail::to($this->user->email)->send(new WelcomeEmail($this->user));
-            Log::info('Correo de bienvenida enviado exitosamente a: ' . $this->user->email);
-        } catch (\Exception $e) {
-            Log::error('Error al enviar correo de bienvenida: ' . $e->getMessage());
-            throw $e;
-        }
     }
 }
